cdbc4f86d8f43edce9d4c96f38ffed27bbdf0fb2
Ayaan Malik <ayaan.malik.in@gmail.com>
2023-11-27 15:40:29 +0530

    index file changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from "react-router-dom";
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
+  <StrictMode>
     <App />
-  </>
+  </StrictMode>
 );
