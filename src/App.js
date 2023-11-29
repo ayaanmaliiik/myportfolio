5c5468a6d504ccb124c408033361bae0565e5411
Ayaan Malik <ayaan.malik.in@gmail.com>
2023-11-29 17:10:30 +0530

    added some animation using framer motion

--- src/App.js
+++ src/App.js
@@ -7,19 +7,33 @@ import Footer from "./components/Footer";
 import Experience from "./components/Experience";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
+import { useRef } from "react";
+import { motion, } from "framer-motion";
 
 function App() {
+  const ref = useRef(null);
+
   return (
-    <div className="">
+    <>
+      <motion.div
+      ref={ref}
+      className="box"
+      transition={{
+        type: "spring",
+        damping: 3,
+        stiffness: 50,
+        restDelta: 0.001
+      }}
+    />
       <Header />
-      <Hero/>
-      <About/>
-      <Skills/>
-      <Projects/>
-      <Experience/>
-      <Contact/>
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Experience />
+      <Contact />
       <Footer />
-    </div>
+    </>
   );
 }
 
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -10,89 +10,93 @@ import MYSQL from "./images/mysql.png";
 import GIT from "./images/git.png";
 import GITHUB from "./images/github.png";
 import GITLAB from "./images/gitlab.png";
+import { motion } from "framer-motion";
 
 const Skills = () => {
   return (
     <section className="skills">
-    <h2 className="pb-2 pt-3">Skills</h2>
+      <h2 className="pb-2 pt-3">Skills</h2>
       <div className="container row">
-        <div className="col-lg-2 col-md-4 col-4 ps-0">
+        <motion.div
+          drag
+          className="col-lg-2 col-md-4 col-4 ps-0"
+        >
           <div className="card border-0">
             <div className="card-body text-center">
               <img src={HTML} />
             </div>
           </div>
-        </div>
-        <div className="col-lg-2 col-md-4 col-4 ps-0">
+        </motion.div>
+        <motion.div drag className="col-lg-2 col-md-4 col-4 ps-0">
           <div className="card border-0">
             <div className="card-body text-center">
               <img src={CSS} />
             </div>
           </div>
-        </div>
-        <div className="col-lg-2 col-md-4 col-4 ps-0">
+        </motion.div>
+        <motion.div drag className="col-lg-2 col-md-4 col-4 ps-0">
           <div className="card border-0">
             <div className="card-body text-center">
               <img src={JS} />
             </div>
           </div>
-        </div>
-        <div className="col-lg-2 col-md-4 col-4 ps-0">
+        </motion.div>
+        <motion.div drag className="col-lg-2 col-md-4 col-4 ps-0">
           <div className="card border-0">
             <div className="card-body text-center">
               <img src={JQUERY} />
             </div>
           </div>
-        </div>
-        <div className="col-lg-2 col-md-4 col-4 ps-0">
+        </motion.div>
+        <motion.div drag className="col-lg-2 col-md-4 col-4 ps-0">
           <div className="card border-0">
             <div className="card-body text-center">
               <img src={BOOTSTRAP} />
             </div>
           </div>
-        </div>
-        <div className="col-lg-2 col-md-4 col-4 ps-0">
+        </motion.div>
+        <motion.div drag className="col-lg-2 col-md-4 col-4 ps-0">
           <div className="card border-0">
             <div className="card-body text-center">
               <img src={REACT} />
             </div>
           </div>
-        </div>
-        <div className="col-lg-2 col-md-4 col-4 ps-0">
+        </motion.div>
+        <motion.div drag className="col-lg-2 col-md-4 col-4 ps-0">
           <div className="card border-0">
             <div className="card-body text-center">
               <img src={PHP} />
             </div>
           </div>
-        </div>
-        <div className="col-lg-2 col-md-4 col-4 ps-0">
+        </motion.div>
+        <motion.div drag className="col-lg-2 col-md-4 col-4 ps-0">
           <div className="card border-0">
             <div className="card-body text-center">
               <img src={MYSQL} />
             </div>
           </div>
-        </div>
-        <div className="col-lg-2 col-md-4 col-4 ps-0">
+        </motion.div>
+        <motion.div drag className="col-lg-2 col-md-4 col-4 ps-0">
           <div className="card border-0">
             <div className="card-body text-center">
               <img src={GIT} />
             </div>
           </div>
-        </div>
-        <div className="col-lg-2 col-md-4 col-4 ps-0">
+        </motion.div>
+        <motion.div drag className="col-lg-2 col-md-4 col-4 ps-0">
           <div className="card border-0">
             <div className="card-body text-center">
               <img src={GITHUB} />
             </div>
           </div>
-        </div>
-        <div className="col-lg-2 col-md-12 col-4 ps-0">
+        </motion.div>
+        <motion.div drag className="col-lg-2 col-md-12 col-4 ps-0">
           <div className="card border-0">
             <div className="card-body text-center">
               <img src={GITLAB} />
             </div>
           </div>
-        </div>
+        </motion.div>
         <hr />
       </div>
     </section>
